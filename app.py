9ed7972be50f867c4c066189a18b1b633a0fa625
Lorenzo <lorenzo.massa@stud.unifi.it>
2023-06-06 23:38:10 +0200

    db updated

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def table_creation():
     conn = db_connection()
     cursor = conn.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS user( id integer PRIMARY KEY, personal_id text NOT NULL, firstname text NOT NULL, lastname text NOT NULL)")
+        "CREATE TABLE IF NOT EXISTS user (id integer PRIMARY KEY, personal_id text NOT NULL, firstname text NOT NULL, lastname text NOT NULL)")
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS monument( id integer PRIMARY KEY, name text NOT NULL, description text NOT NULL, category text NOT NULL, image text NOT NULL)")
+        "CREATE TABLE IF NOT EXISTS monument (id integer PRIMARY KEY, name text NOT NULL, description text NOT NULL, category text NOT NULL, image text NOT NULL)")
     cursor.execute(
         "CREATE TABLE IF NOT EXISTS interaction (user_id integer NOT NULL,monument_id integer NOT NULL,PRIMARY KEY (user_id, monument_id),FOREIGN KEY (user_id) REFERENCES users(id),FOREIGN KEY (monument_id) REFERENCES monuments(id))")
     cursor.execute(
